8a7262b3b58952e719774ea2ab4e639ebf67dfaf
aamina-ctrl <aaminaanwarsheikh@gmail.com>
2026-07-14 12:18:47 +0400

    Add files via upload

--- level3_security_utilities/url_parser/url_parser.py
+++ level3_security_utilities/url_parser/url_parser.py
@@ -1,1 +1,35 @@
+from urllib.parse import urlparse, parse_qs
+def validate_url(url):
+    parsed= urlparse(url)
+    if not parsed.scheme or not parsed.hostname:
+        print("Invalid URL: Missing protocol or hostname.")
+        return None
+    else:
+        return parsed
+    
+def display_components(parsed):
+    print(f"Protocol: {parsed.scheme}")
+    print(f"Hostname: {parsed.hostname}")
+    print(f"Path: {parsed.path}")
+    print(f"Port: {parsed.port}")
+    print(f"Query Parameters: {parsed.query}")
+    print(f"Fragment: {parsed.fragment}")
 
+    if parsed.scheme=="https":
+        print("HTTPS: Enabled")
+    else:
+        print("HTTPS: Not enabled")
+
+    parameters=parse_qs(parsed.query)
+    if parameters:
+        print("Query Parameters:")
+        for key, value in parameters.items():
+            print(f"  {key}: {value}")
+        print(f"Number of Query Parameters: {len(parameters)}")
+
+def main():
+    url=input("Enter a VALID URL:")
+    parsed=validate_url(url)
+    if parsed:
+        display_components(parsed)
+main()    
